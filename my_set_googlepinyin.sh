ef9470f27a4d0a8a54015201d4c47a30dc21423d
gf9276 <927621609@qq.com>
2023-05-20 11:28:14 +0800

    优化逻辑

--- my_set_googlepinyin.sh
+++ my_set_googlepinyin.sh
@@ -5,27 +5,32 @@ set -o errexit # 报错直接退出
 echo -e "\e[36msudo会使代理失效, 建议以root用户的身份开启代理后再执行\e[0m"
 echo -e "\e[36m开始更新、安装、设置语言\e[0m"
 
+fcitx_cfg_file_path="/etc/profile.d/my_fcitx.sh"
+
 # 更新、安装fcitx和cjk、生成机器码
 apt -y update
 apt -y install fcitx fonts-noto-cjk fonts-noto-color-emoji dbus-x11 fcitx-googlepinyin
 dbus-uuidgen >/var/lib/dbus/machine-id
 
+echo ""
+echo -e "\e[36m安装完成, 写入环境变量到:\e[93m$fcitx_cfg_file_path\e[0m"
+
 # 检查文件是否存在，这个文件只能是我自己写的
-if [ -e "/etc/profile.d/my_fcitx.sh" ]; then # 存在就删掉吧，重新来
-    rm -f /etc/profile.d/my_fcitx.sh
+if [ -e $fcitx_cfg_file_path ]; then # 存在就删掉吧，重新来
+    rm -f $fcitx_cfg_file_path
 fi
 
 # 写入环境变量
-echo '#!/bin/bash' >>/etc/profile.d/my_fcitx.sh
-echo '' >>/etc/profile.d/my_fcitx.sh
-echo '# 设置默认输入方式' >>/etc/profile.d/my_fcitx.sh
-echo 'export QT_IM_MODULE=fcitx' >>/etc/profile.d/my_fcitx.sh
-echo 'export GTK_IM_MODULE=fcitx' >>/etc/profile.d/my_fcitx.sh
-echo 'export XMODIFIERS=@im=fcitx' >>/etc/profile.d/my_fcitx.sh
-echo 'export DefaultIMModule=fcitx' >>/etc/profile.d/my_fcitx.sh
-echo '' >>/etc/profile.d/my_fcitx.sh
-echo '# fcitx自启动' >>/etc/profile.d/my_fcitx.sh
-echo 'fcitx-autostart &>/dev/null' >>/etc/profile.d/my_fcitx.sh
+echo '#!/bin/bash' >>$fcitx_cfg_file_path
+echo '' >>$fcitx_cfg_file_path
+echo '# 设置默认输入方式' >>$fcitx_cfg_file_path
+echo 'export QT_IM_MODULE=fcitx' >>$fcitx_cfg_file_path
+echo 'export GTK_IM_MODULE=fcitx' >>$fcitx_cfg_file_path
+echo 'export XMODIFIERS=@im=fcitx' >>$fcitx_cfg_file_path
+echo 'export DefaultIMModule=fcitx' >>$fcitx_cfg_file_path
+echo '' >>$fcitx_cfg_file_path
+echo '# fcitx自启动' >>$fcitx_cfg_file_path
+echo 'fcitx-autostart &>/dev/null' >>$fcitx_cfg_file_path
 
 echo ""
 echo -e "\e[36m安装结束, wsl --shutdown 后重启wsl\e[0m"
