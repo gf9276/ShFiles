aa20510f605e4337b2eba7c5199d0b62df825e93
gf9276 <927621609@qq.com>
2023-06-10 16:26:52 +0800

    更新一下代理设置

--- my_proxy.sh
+++ my_proxy.sh
@@ -8,14 +8,15 @@ socks_port=7890 # 看clash的配置，混合模式默认用的确实是7890
 PROXY_HTTP="http://${hostip}:${port}"
 PROXY_SOCKS="socks5h://${hostip}:${socks_port}"
 
-set_proxy(){
+set_proxy() {
   export all_proxy="${PROXY_SOCKS}"
   export ALL_PROXY="${PROXY_SOCKS}"
   export https_proxy="${PROXY_HTTP}"
   export HTTPS_PROXY="${PROXY_HTTP}"
   export http_proxy="${PROXY_HTTP}"
   export HTTP_PROXY="${PROXY_HTTP}"
-  export no_proxy="localhost,127.0.0.1,${hostip},${wslip}"
+  export no_proxy="localhost, 127.0.0.0/8, ::1, ${hostip}, ${wslip}"
+  export NO_PROXY="localhost, 127.0.0.0/8, ::1, ${hostip}, ${wslip}"
 
   git config --global http.https://github.com.proxy ${PROXY_SOCKS}
   git config --global https.https://github.com.proxy ${PROXY_SOCKS}
@@ -23,13 +24,15 @@ set_proxy(){
   echo "Proxy has been opened."
 }
 
-unset_proxy(){
+unset_proxy() {
   unset ALL_PROXY
   unset all_proxy
   unset HTTPS_PROXY
   unset https_proxy
   unset HTTP_PROXY
   unset http_proxy
+  unset NO_PROXY
+  unset no_proxy
 
   git config --global --unset http.https://github.com.proxy
   git config --global --unset https.https://github.com.proxy
@@ -37,7 +40,7 @@ unset_proxy(){
   echo "Proxy has been closed."
 }
 
-test_setting(){
+test_setting() {
   echo "Host IP:" ${hostip}
   echo "WSL IP:" ${wslip}
   echo "Try to connect to Google..."
@@ -52,16 +55,13 @@ test_setting(){
 }
 
 v1=${1:-"set"} # 设置默认为set，即开启
-if [ "$v1" = "set" ]
-then
+if [ "$v1" = "set" ]; then
   set_proxy
 
-elif [ "$v1" = "unset" ]
-then
+elif [ "$v1" = "unset" ]; then
   unset_proxy
 
-elif [ "$v1" = "test" ]
-then
+elif [ "$v1" = "test" ]; then
   test_setting
 else
   echo "Unsupported arguments."
