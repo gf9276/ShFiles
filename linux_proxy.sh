af94d151d922189cf7a99d43588975d53772f60a
gf9276 <927621609@qq.com>
2024-01-24 16:20:00 +0800

    修复

--- linux_proxy.sh
+++ linux_proxy.sh
@@ -39,8 +39,8 @@ unset_proxy() {
   unset NO_PROXY
   unset no_proxy
 
-  conda config --remove-key proxy_servers.http
-  conda config --remove-key proxy_servers.https
+  conda config --remove-key proxy_servers
+  conda config --remove-key proxy_servers
 
   git config --global --unset http.https://github.com.proxy
   git config --global --unset https.https://github.com.proxy
